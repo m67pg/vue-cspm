a3bf4cc4721ddff735d36114b3884ec269a94995
m67pg <m_owaki@nifty.com>
2023-09-07 13:31:15 +0900

    When navigating from a project to project details, the current page of the project may not be saved correctly.

--- src/views/project_detail/Index.vue
+++ src/views/project_detail/Index.vue
@@ -45,9 +45,15 @@ export default {
     mounted() {
         this.isLoading = true;
 
-        if (this.prevRoute.path === '/project') {
+        console.log(this.prevRoute.path);
+        console.log(this.$route.query.page);
+        console.log(this.$store.state.page);
+
+        if (this.prevRoute.path === '/' || this.prevRoute.path === '/project') {
             this.$store.commit('savePage', this.$route.query.page);
         }
+
+        console.log(this.$store.state.page);
 
         axios
         .get('/api/project/' + this.projectId() + '/project_detail')
